9aba022d1fd893e0514d0fdc00c5248ce33a905c
adtreclove <helenakloeckner1@gmail.com>
2023-05-03 14:33:00 +0200

    Update CamController
    Switch between follow camera and normal camera with edge scrolling possible

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,6 +1,7 @@
 using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,14 +11,20 @@ public class CameraController : MonoBehaviour
     //this script is for unfocused camera, just to look around map
 
     private WarlordController inputAction;
-    
     private CinemachineBrain cinemachineBrain;
+
+
+    private float screenWidth;
+    private float boundary = 20f;
+    private float speed = 70f;
 
     private void Awake()
     {
         inputAction = new WarlordController();
        
         cinemachineBrain = GetComponent<CinemachineBrain>();
+
+        screenWidth = Screen.width;
     }
 
     private void OnEnable()
@@ -33,6 +40,12 @@ public class CameraController : MonoBehaviour
 
     private void Update()
     {
+        if(cinemachineBrain.enabled == false)
+        {
+           
+            CameraMove();
+        }
+      
         //if y is pressed, the camera should switch between focused and unfocused (cinemachine brain on or off)
 
         if(inputAction != null)
@@ -40,6 +53,7 @@ public class CameraController : MonoBehaviour
             if(inputAction.W_Controller.CameraFocus.WasPressedThisFrame() && cinemachineBrain.enabled == true)
             {
                 cinemachineBrain.enabled = false;
+                
             }
             else if(inputAction.W_Controller.CameraFocus.WasPressedThisFrame() && cinemachineBrain.enabled == false)
             {
@@ -48,4 +62,24 @@ public class CameraController : MonoBehaviour
         }
 
     }
+
+    private void CameraMove()
+    {
+        //move camera in x direction if mouse is pushing out of screen boundaries
+        //mouse position, main camera x transform
+
+        if(Input.mousePosition.x > screenWidth - boundary)
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+            
+        }
+        if (Input.mousePosition.x < boundary)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+
+        }
+
+    }
+
+
 }
