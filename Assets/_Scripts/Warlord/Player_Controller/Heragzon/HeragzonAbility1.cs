e7a8af3750e209b2721b13ae861e38626725c285
K4K4tz3 <88493804+K4K4tz3@users.noreply.github.com>
2023-05-22 16:30:09 +0200

    added Package + Folder rename
    added Package:
    - Netcode for GameObjects(unity)
    
    renamed:
    - Scripts -> _Scripts

---
+++ Assets/_Scripts/Warlord/Player_Controller/Heragzon/HeragzonAbility1.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeragzonAbility1 : MonoBehaviour
+{
+    [SerializeField] private WarlordBaseClass heragzonSO;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("hello");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+
+        //damage wird jz jeden frame gemacht, ändern auf pro sekunde 
+        var tag = other.tag;
+        if (other.gameObject.TryGetComponent(out IDamagable d))
+        {
+            switch (tag)
+            {
+                case "Building":
+                    d.GetDamaged(heragzonSO.ability1DmgBuilding);
+                    Debug.Log("ability 1 trigger stay");
+                    break;
+                case "Warlord":
+                    d.GetDamaged(heragzonSO.ability1DmgWarlord);
+                    if (other.gameObject.TryGetComponent(out IStunnable w))
+                    {
+                        w.GetStunned(heragzonSO.ability1Duration);
+                    }
+                    Debug.Log("ability 1 trigger stay");
+                    break;
+                case "Minion":
+                    d.GetDamaged(heragzonSO.ability1DmgMinion);
+                    if (other.gameObject.TryGetComponent(out IStunnable m))
+                    {
+                        m.GetStunned(heragzonSO.ability1Duration);
+                    }
+                    Debug.Log("ability 1 trigger stay");
+                    break;
+            }
+        }
+    }
+
+}
